30a22f95c48fd485bb261f3e616125b15a5a6b30
mathis-intcla <51505831+mathis-intcla@users.noreply.github.com>
2025-06-05 14:59:57 +0200

    Configure dynamic deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   // App Router is enabled by default in Next.js 14
   eslint: {
     ignoreDuringBuilds: true,
